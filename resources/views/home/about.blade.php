99acfa98011df0397b4b371505967000fdb407c9
nimasayuu <nimasayusafitri8@gmail.com>
2025-01-20 13:51:09 +0700

    app_hotel_hebat

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,18 @@
+<div class="about">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-5">
+                  <div class="titlepage">
+                     <h2>About Us</h2>
+                     <p>Selamat datang di Hotel Hebat, tempat di mana kenyamanan, kehangatan, dan layanan terbaik berpadu untuk menciptakan pengalaman menginap yang tak terlupakan. Kami berdedikasi untuk memberikan pelayanan kelas dunia dengan sentuhan lokal, menjadikan setiap tamu merasa istimewa sejak saat pertama mereka tiba. </p>
+                     <a class="read_more" href="Javascript:void(0)"> Read More</a>
+                  </div>
+               </div>
+               <div class="col-md-7">
+                  <div class="about_img">
+                     <figure><img src="images/about.png" alt="#"/></figure>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
